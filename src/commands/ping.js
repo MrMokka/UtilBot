ceee66ba1a1e2c51129181a93e79e14918dcce7e
MrMokka <sartaren@hotmail.com>
2020-11-30 18:49:46 +0100

    fixed typo for help, added cmopleted and hidden

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,9 @@
 module.exports = {
+	completed: true,
+	hidden: false,
 	name: 'ping',
-	description: 'Ping!',
+	description: 'Ping Pong!',
+	usage: 'ping',
 	execute(message, args) {
 		message.channel.send('Pong.');
 	},
